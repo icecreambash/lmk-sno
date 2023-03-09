9f0b4833d00bfba7a17d5cf72d4706081383190f
icecreambash <artemcsgo48rus@gmail.com>
2023-03-09 23:34:23 +0300

    rpops

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,5 @@
     @include('layouts.navbar')
     {{$slot}}
     @livewireScripts
-    <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js" data-turbolinks-eval="false" data-turbo-eval="false"></script>
 </body>
 </html>
